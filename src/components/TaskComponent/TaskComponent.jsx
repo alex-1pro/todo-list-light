40adb0499730195147a59c162dfbb2abe7b14aa0
alex-1pro <pro1.alex.b@gmail.com>
2021-08-03 17:00:49 +0300

    adding removeTask function

--- src/components/TaskComponent/TaskComponent.jsx
+++ src/components/TaskComponent/TaskComponent.jsx
@@ -1,23 +1,32 @@
 import './TaskComponent.css';
 import React from 'react';
 import { useState } from 'react';
-
-function TaskComponent({ tasks, task, idNum, onChecked, setTasks }) {
-
-
-    // function changeDone(){
-    //     console.log("in func changeDone()"+ idNum);
-    //     onChecked(idNum);
-    // }
+import { Col, Container, Row } from 'react-bootstrap';
+function TaskComponent({ tasks, task, idNum, onChecked, onRemove }) {
 
 
+    
+
+    // const trashlogo = <><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+        {/* <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" /> */}
+    {/* </svg></> */}
 
     return (
-        <div className="task-row">
-            <input type="checkbox" id={"box-" + idNum} onChange={() => onChecked(idNum)} />
-            <label for={"box-" + idNum} className={task.done ? "done" : ""}>{task.task}</label>
 
-        </div>
+       <div className="c-task-row">
+   
+            <div class=" task-col">
+                <input type="checkbox" id={"box-" + idNum} onChange={() => onChecked(idNum)} />
+            
+
+                <label for={"box-" + idNum} className={task.done ? "done" : ""}>{task.task} </label>
+            </div>
+            <div class=" task-col">
+                <span onClick={()=>onRemove(idNum)} className="cont-trash"><img src="https://img.icons8.com/material-rounded/24/000000/trash.png" alt="" /></span>
+            </div>
+           
+    </div>
+
     );
 }
 
--- src/pages/TodoPage/TodoPage.jsx
+++ src/pages/TodoPage/TodoPage.jsx
@@ -14,7 +14,7 @@ function TodoPage() {
             const newTask = new TaskModel(taskText, false);
             setTasks(tasks.concat(newTask));
             setTaskText("");
-            
+
         }
     }
 
@@ -23,28 +23,41 @@ function TodoPage() {
         let tempTasks = [...tasks];
         tempTasks[idNum].done = !tempTasks[idNum].done;
         setTasks(tempTasks);
-    
+
     }
+
+    function removeTask(idNum){
+        let tempTasks = [...tasks];
+        tempTasks.splice(idNum,1);
+        setTasks(tempTasks);
+    }
+
+
     // function  incompleteTasks(){
     //     const count = tasks.filter(t => t.done === false);
     //     setTaskCount(count.length);
     // }
-    const incompleteTasks=tasks.filter(t => t.done === false).length;
-        console.log(tasks.filter(t => t.done === false).length);
-    const tasksList = tasks.map((tsk, index) => (
-        <TaskComponent tasks={tasks} task={tsk} idNum={index} onChecked={taskDone} setTasks={setTasks} />)
-    )
-    return (
-        <Container >
-            <h1>Todo List</h1>
-            <Row>
-                <input type="text" value={taskText} placeholder="Enter new Task" onChange={e => setTaskText(e.target.value)} />
+    const incompleteTasks = tasks.filter(t => t.done === false).length;
 
-                <button onClick={addToList}>New Task</button>
-            </Row>
-            {tasksList}
-            <p className="coun-tasks">{incompleteTasks} items left</p>
-        </Container>
+    const tasksList = tasks.map((tsk, index) => (
+        <TaskComponent tasks={tasks} task={tsk} idNum={index} onChecked={taskDone}  onRemove={removeTask}/>)
+    )
+   
+    return (
+        <div className="c-todopage">
+            <Container >
+                <h1>Todo List</h1>
+                <Row>
+                    <input type="text" value={taskText} placeholder="Enter new Task" onChange={e => setTaskText(e.target.value)} />
+
+                    <button onClick={addToList}>New Task</button>
+                </Row>
+                {/* <ul className="ul-todolist"> */}
+                    {tasksList}
+                {/* </ul> */}
+                <p className="coun-tasks">{incompleteTasks} items left</p>
+            </Container>
+        </div>
     );
 }
 
