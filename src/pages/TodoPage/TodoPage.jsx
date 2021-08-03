cb737596e2225d10aabd3b824c3e21fe46303bf7
alex-1pro <pro1.alex.b@gmail.com>
2021-08-03 21:46:14 +0300

    adding filter

--- src/pages/TodoPage/TodoPage.jsx
+++ src/pages/TodoPage/TodoPage.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import TaskComponent from '../../components/TaskComponent/TaskComponent';
 import { useState } from 'react';
 import TaskModel from '../../model/TaskModel';
-import { Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function TodoPage() {
     const [tasks, setTasks] = useState([]);
     const [taskText, setTaskText] = useState("");
-    const [taskCount, setTaskCount] = useState(0);
+    const [filteredTasks, setFilteredTasks] = useState([]);
+    const [filterType, setFilterType] = useState("All");
     function addToList() {
         if (taskText) {
             const newTask = new TaskModel(taskText, false);
@@ -32,37 +33,83 @@ function TodoPage() {
         setTasks(tempTasks);
     }
 
-  
-  
+
+
     function handleKeyDown(e) {
         if (e.key === 'Enter') {
-          console.log('do validate');
-          addToList();
+            console.log('do validate');
+            addToList();
         }
-      }
+    }
+    function displayAllTask() {
+        return tasksList;
+    }
+
+    function displayCompletTaska() {
+        const completed = tasks.filter(t => t.done);
+        return completed.map((tsk, index) => (
+            <TaskComponent tasks={completed} task={tsk} idNum={index} onChecked={taskDone} onRemove={removeTask} />)
+        )
+    }
+    function displayActiveTasks() {
+        const incompleted = tasks.filter(t => t.done === false);
+        return incompleted.map((tsk, index) => (
+            <TaskComponent tasks={incompleted} task={tsk} idNum={index} onChecked={taskDone} onRemove={removeTask} />)
+        )
+    }
 
     const incompleteTasks = tasks.filter(t => t.done === false).length;
 
-    const tasksList = tasks.map((tsk, index) => (
-        <TaskComponent tasks={tasks} task={tsk} idNum={index} onChecked={taskDone} onRemove={removeTask} />)
-    )
+    function filterTask() {
+        if (filterType === "All") {
+            return tasks.map((tsk, index) => (
+                <TaskComponent tasks={tasks} task={tsk} idNum={index} onChecked={taskDone} onRemove={removeTask} />)
+            )
+        } else if (filterType === "Active") {
+            const active = tasks.filter(t => t.done === false);
+            return active.map((tsk, index) => (
+                <TaskComponent tasks={active} task={tsk} idNum={index} onChecked={taskDone} onRemove={removeTask} />)
+            )
+        } else {
+            const doneTasks = tasks.filter(t => t.done);
+            return doneTasks.map((tsk, index) => (
+                <TaskComponent tasks={doneTasks} task={tsk} idNum={index} onChecked={taskDone} onRemove={removeTask} />)
+            )
+        }
 
+    }
+    const tasksList = filterTask();
+
+    // const tasksList = tasks.map((tsk, index) => (
+    // <TaskComponent tasks={tasks} task={tsk} idNum={index} onChecked={taskDone} onRemove={removeTask} />)
+    // )
+    console.log(filterType);
     return (
         <div className="c-todopage">
             <Container >
                 <h1>Todo List</h1>
-                <Row>
-                <input type="text" value={taskText} placeholder="Enter new Task" onChange={e => setTaskText(e.target.value)} onKeyDown={handleKeyDown}/>
-                    {/* <input type="text" value={taskText} placeholder="Enter new Task" onChange={e => setTaskText(e.target.value)} /> */}
+                <Row >
 
+                    <input className="input-task" type="text" value={taskText} placeholder="Enter new Task" onChange={e => setTaskText(e.target.value)} onKeyDown={handleKeyDown} />
+                    {/* <input type="text" value={taskText} placeholder="Enter new Task" onChange={e => setTaskText(e.target.value)} /> */}
+                    <Col>
+                        <div className={filterType === "All" ? "m-press" : "" + " my-btn "} onClick={() => setFilterType("All")}>All</div>
+
+                    </Col>
+                    <Col>
+                        <div className={filterType === "Active" ? "m-press" : "" + " my-btn "} onClick={() => setFilterType("Active")}>Active</div>
+                    </Col>
+                    <Col>
+                        <div className={filterType === "Completed" ? "m-press" : ""  + " my-btn "} onClick={() => setFilterType("Completed")}>Completed</div>
+                    </Col>
                     {/* <button onClick={addToList}>New Task</button> */}
+                    {incompleteTasks > 0 ? <p className="coun-tasks">{incompleteTasks} items left</p> : <p className="coun-tasks">No tasks to do</p>}
                 </Row>
-                {/* <ul className="ul-todolist"> */}
+
                 {tasksList}
-                {/* </ul> */}
-                <p className="coun-tasks">{incompleteTasks} items left</p>
+
             </Container>
-         
+
         </div>
     );
 }
